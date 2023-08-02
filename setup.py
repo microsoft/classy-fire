d7626418a6e6f3a76a3f490236ed84474cb4c33e
Shay Ben-Elazar <shbenela@microsoft.com>
2023-08-02 15:39:29 +0300

    setup bugfix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['tiktoken, langchain, openai'],
+    install_requires=['tiktoken', 'langchain', 'openai'],
     keywords=['pypi', 'cicd', 'python'],
     classifiers=[
         "Development Status :: 1 - Planning",
