5b4e4af0e3deb49a439dcd43a9477a279ec9ad22
Shay Ben-Elazar <shbenela@microsoft.com>
2023-09-04 16:30:06 +0300

    Ran black formatter

--- classy_fire.py
+++ classy_fire.py
@@ -1,11 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
-    
+
 import os
 import tiktoken
 from typing import Tuple
 from langchain.chat_models import AzureChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
+
 
 class LLMClassifier:
     """
@@ -19,6 +20,7 @@ class LLMClassifier:
         classifier("Shines")
         > "Stars"
     """
+
     MAX_TOKEN_PRIOR: int = 100
 
     system_prompt_base: str = (
@@ -28,41 +30,60 @@ class LLMClassifier:
         "<options>\n{options_string}\n</options>\n"
         "\nHere are some examples of how to respond:\n{few_shot_examples}\n\n###\n"
     )
-    user_prompt_base: str = (
-        "<Input>\n{user_prompt}\n</Input>\nResponse: "
-    )
+    user_prompt_base: str = "<Input>\n{user_prompt}\n</Input>\nResponse: "
 
-    def __init__(self, 
-                 class_names: list[str], 
-                 task_description: str = "",
-                 few_shot_examples: str = "", 
-                 deployment_name: str = "gpt-35-turbo-0301", 
-                 model_name: str = "gpt-3.5-turbo") -> None:
+    def __init__(
+        self,
+        class_names: list[str],
+        task_description: str = "",
+        few_shot_examples: str = "",
+        deployment_name: str = "gpt-35-turbo-0301",
+        model_name: str = "gpt-3.5-turbo",
+    ) -> None:
         self.class_names = class_names
         tokenizer = tiktoken.encoding_for_model(model_name)
         class_id_tokens = [tokenizer.encode(str(i)) for i in range(len(class_names))]
-        assert all(len(token_list) == 1 for token_list in class_id_tokens), \
-            f"A class id number did not map to a single token, maybe too many classes in list: {len(class_names)}"
+        assert all(
+            len(token_list) == 1 for token_list in class_id_tokens
+        ), f"A class id number did not map to a single token, maybe too many classes in list: {len(class_names)}"
         self.system_message = SystemMessage(
             content=self.system_prompt_base.format(
-                context_string = task_description,
-                options_string="\n".join([f"{i}. {cls}" for i, cls in zip(range(len(class_names)), class_names)]),
-                few_shot_examples=few_shot_examples
-        ))
-        self._logit_bias = {token_list[0]: LLMClassifier.MAX_TOKEN_PRIOR for token_list in class_id_tokens}
-        self._llm_chat = AzureChatOpenAI(deployment_name=deployment_name,
-                                         openai_api_base=os.environ.get("OPENAI_API_BASE"),
-                                         openai_api_version=os.environ.get("OPENAI_API_VERSION"),
-                                         openai_api_key=os.environ.get("OPENAI_API_KEY"),
-                                         temperature=0,
-                                         max_tokens=1,
-                                         model_kwargs={"logit_bias": self._logit_bias})
+                context_string=task_description,
+                options_string="\n".join(
+                    [
+                        f"{i}. {cls}"
+                        for i, cls in zip(range(len(class_names)), class_names)
+                    ]
+                ),
+                few_shot_examples=few_shot_examples,
+            )
+        )
+        self._logit_bias = {
+            token_list[0]: LLMClassifier.MAX_TOKEN_PRIOR
+            for token_list in class_id_tokens
+        }
+        self._llm_chat = AzureChatOpenAI(
+            deployment_name=deployment_name,
+            openai_api_base=os.environ.get("OPENAI_API_BASE"),
+            openai_api_version=os.environ.get("OPENAI_API_VERSION"),
+            openai_api_key=os.environ.get("OPENAI_API_KEY"),
+            temperature=0,
+            max_tokens=1,
+            model_kwargs={"logit_bias": self._logit_bias},
+        )
 
     def __call__(self, input_string: str) -> Tuple[str, int]:
         """
         The function takes a string and classifies it to a class name and class id.
         """
-        response = self._llm_chat([self.system_message, HumanMessage(content=self.user_prompt_base.format(user_prompt=input_string))])
+        response = self._llm_chat(
+            [
+                self.system_message,
+                HumanMessage(
+                    content=self.user_prompt_base.format(user_prompt=input_string)
+                ),
+            ]
+        )
         predicted_class_id = int(response.content)
 
         return self.class_names[predicted_class_id], predicted_class_id
